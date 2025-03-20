e094269a21aa8362d320c98740da7526abd1853d
Tushar Munge <63478179+Satch-Satchafunkilus@users.noreply.github.com>
2025-03-20 12:10:34 -0500

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Tushar Munge on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
